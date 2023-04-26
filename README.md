279ae7f057a286ffc8c35e5a334a1e8347b6a5c3
Armando Soares Sousa <armando.sousa@gmail.com>
2023-04-26 19:27:07 +0000

    Testes de analise de commits e arquivos.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mypod
 My gitpod to test
+
+## Instala dependencias
+$pip install -r requirements
---
+++ limpa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Clear the terminal
+clear
+
+rm -rf __pycache__
+rm -rf promocity
+rm my_promocity.db
